c7fc6092f4cc34ebb71eb9543a93db42bcfcc355
adrq <adrq22@yahoo.com>
2017-01-03 14:42:05 -0500

    update database schema

--- db.sql
+++ db.sql
@@ -1,22 +1,21 @@
 /* main table for records*/
 CREATE TABLE records (
   id int(11) NOT NULL AUTO_INCREMENT,
+  mid int(11),
   title text,
-  publisher int(11),
-  year varchar(255),
+  publisher varchar(255),
   call_number varchar(255),
   series varchar(255),
   larger_work varchar(255),
   collection_source varchar(255),
-  donor text varchar(255),
-  notes text varchar(255),
+  donor varchar(255),
   scanning_technician varchar(255),
   media_cataloguer varchar(255),
   reviewer varchar(255),
   PRIMARY KEY (id)
 );
 
-/*
+/* 
  * additional tables
  */
 CREATE TABLE alternative_titles (
@@ -26,15 +25,40 @@ CREATE TABLE alternative_titles (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE years (
+  record_id int(11),
+  start_year varchar(10),
+  end_year varchar(10),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE notes (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  record_id int(11),
+  note text,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE contributors (
   id int(11) NOT NULL AUTO_INCREMENT,
   record_id int(11) NOT NULL,
-  contributor varchar(255),
-  role varchar(255),
-  /* This is where we would store the contributor URI if we have one */
-  contributor_uri text,
+  contributor_id int(11) NOT NULL,
+  role_id varchar(255),
   PRIMARY KEY (id)
 );
+
+CREATE TABLE roles (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  role varchar(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE tbl_name (
+  name varchar(255),
+  uri varchar(2000),,
+  nameUpdate varchar(100),
+  localID int(10) UNSIGNED NOT NULL
+)
 
 CREATE TABLE publisher_locations (
   id int(11) NOT NULL AUTO_INCREMENT,
@@ -46,7 +70,7 @@ CREATE TABLE publisher_locations (
 CREATE TABLE texts (
   id int(11) NOT NULL AUTO_INCREMENT,
   record_id int(11) NOT NULL,
-  text text,
+  text_t text,
   PRIMARY KEY (id)
 );
 
