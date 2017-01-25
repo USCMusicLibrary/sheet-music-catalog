14140c9c6cd8735dd9db13fe8ecc2572b07ec6f8
jsharv <jsharvey55@gmail.com>
2017-01-25 10:43:20 -0500

    adding support for photographers and editors
    I'm not sure we'll end up wanting to keep both of these, but it
    shouldn't take Ana & Jen to come to a decision about it once they see
    them in action. So, they're here for now. Also, I took uploads\ out of
    my gitignore so we'll be working with the same backup file

--- config.php
+++ config.php
@@ -49,6 +49,12 @@ $solrFieldNames = array(
 "illustrator" => array("field_title" => "Illustrator",
 	"display" => "full"
 ),
+"photographer" => array("field_title" => "Photographer",
+                      "display" => "full"
+),
+"editor" => array("field_title" => "Editor",
+                      "display" => "full"
+),
 "publisher_location" => array("field_title" => "Publisher location",
 	"display" => "full"
 ),
@@ -102,7 +108,8 @@ $facetFields = array(
 		"lyricist_facet" => "Lyricist",
 		"arranger_facet" => "Arranger",
 		"illustrator_facet"=> "Illustrator",
-		//"photographer_facet" => "Photographer",
+		"photographer_facet" => "Photographer",
+                                    "editor_facet"  => "Editor",
 		"publisher_facet" => "Publisher",
 		"publisher_location_facet" => "Publisher Location",
 		"subject_heading_facet" => "LC Subject Headings",
@@ -121,7 +128,8 @@ $searchFields = array(
 'lyricist',
 'arranger',
 'illustrator',
-'other_contributor',
+'photographer',
+'editor',
 'years_text',
 'language',
 'text_t',
