031a6c617b6cfe15bfe72e0a2f3150f6becf4655
adrq <adrq22@yahoo.com>
2017-05-09 14:53:31 -0400

    added db query for account page

--- admin/account.php
+++ admin/account.php
@@ -18,6 +18,32 @@ require "admin-navigation.php";
   <div class="row">
     <div class="col-xs-12">
       <h1>Account info goes here </h1>
+      <?php
+        $statement = $mysqli->prepare("SELECT * FROM users WHERE username=? LIMIT 1");
+        $statement->bind_param('s',$_SESSION['username']);
+        $statement->execute();
+        $metaResults = $statement->result_metadata();
+    $fields = $metaResults->fetch_fields();
+    $statementParams='';
+     //build the bind_results statement dynamically so I can get the results in an array
+     //because I'm lazy'
+     foreach($fields as $field){
+         if(empty($statementParams)){
+             $statementParams.="\$column['".$field->name."']";
+         }else{
+             $statementParams.=", \$column['".$field->name."']";
+         }
+    }
+    $statment="\$statement->bind_result($statementParams);";
+    eval($statment);
+    while($statement->fetch()){
+      foreach ($column as $key=>$value):
+        if ($key=='password_hash') continue;
+        ?>
+        <p><strong><?php print $key;?>: </strong><?php print $value;?></p>
+      <?php endforeach;
+    }
+      ?>
     </div>
   </div>
 </div> <!-- container-fluid -->
--- admin/admin-navigation.php
+++ admin/admin-navigation.php
@@ -1,12 +1,12 @@
 <?php
 /* this file contains navigation elements for the admin pages */
 ?>
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-inverse">
       <div class="container">
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li><a href="$ROOTURL/admin/index">Start</a></li>
-            <li><a href="$ROOTURL/admin/all">All</a></li>
+            <li><a href="<?php print $ROOTURL;?>admin/index">Start</a></li>
+            <li><a href="<?php print $ROOTURL;?>admin/all">All</a></li>
             <?php if (isLoggedIn()):?><li><a href="logout">Log out</a></li><?php endif;?>
           </ul>
         </div><!--/.nav-collapse -->
